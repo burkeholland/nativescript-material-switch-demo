51123def45d814764299a65c106beaa26efb7904
Burke Holland <burkeholland@gmail.com>
2015-12-02 19:18:34 -0600

    Removing node_modules and platforms folders

--- platforms/ios/Pods/Target Support Files/JTMaterialSwitch/JTMaterialSwitch-umbrella.h
+++
@@ -1,7 +0,0 @@
-#import <UIKit/UIKit.h>
-
-#import "JTMaterialSwitch.h"
-
-FOUNDATION_EXPORT double JTMaterialSwitchVersionNumber;
-FOUNDATION_EXPORT const unsigned char JTMaterialSwitchVersionString[];
-
--- platforms/ios/build/emulator/demo.app/app/app.js
+++
@@ -1,9 +0,0 @@
-var application = require("application");
-application.mainModule = "main-page";
-application.cssFile = "./app.css";
-try {
-	application.start();
-}
-catch (err) {
-	console.log(err);
-}
